6cdaf0e67dc30dcaa21d041bb68467ec33c699fa
Rufat00 <rufat.niftaliyev2007@gmail.com>
2023-08-08 12:45:07 +0400

    areTextSimilar not working

--- client/helpers/areTextsSimilar.js
+++ client/helpers/areTextsSimilar.js
@@ -1,16 +1,23 @@
 export default function areTextsSimilar(text1, text2, threshold) {
-    const minLength = Math.min(text1.length, text2.length);
     let differingCount = 0;
 
-    for (let i = 0; i < minLength; i++) {
+    if (text1.length > text2.length) {
+        text1.slice(0, text2.length);
+        differingCount += text1.length - text2.length;
+    }
+    if (text2.length > text1.length) {
+        text2.slice(0, text1.length);
+        differingCount += text2.length - text1.length;
+    }
+
+    for (let i = 0; i < text1.length; i++) {
         if (text1[i] !== text2[i]) {
             differingCount++;
-            if (differingCount >= threshold) {
+            if (differingCount > threshold) {
                 return false;
             }
         }
     }
 
-    differingCount += Math.abs(text1.length - text2.length);
     return true;
 }
