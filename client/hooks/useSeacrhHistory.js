97540c19ea9e5b66e04e5a4a724a3f1e7b9b4a21
Rufat00 <rufat.niftaliyev2007@gmail.com>
2023-08-11 02:11:06 +0400

    client fix

--- client/hooks/useSeacrhHistory.js
+++ client/hooks/useSeacrhHistory.js
@@ -1,30 +1,32 @@
 import { useEffect, useState } from "react";
 
 const useSearchHistory = () => {
+    let storage;
     if (typeof localStorage !== "undefined") {
-        const [history, setHistory] = useState(
-            localStorage.getItem("history") ? JSON.parse(localStorage.getItem("history")) : []
-        );
-
-        const add = (query) => {
-            const newHistory = [...history, query];
-            if (newHistory.length > 100) {
-                newHistory.pop();
-            }
-
-            localStorage.setItem("history", JSON.stringify(newHistory));
-            setHistory(newHistory);
-        };
-
-        const remove = (query) => {
-            const newHistory = history.filter((value) => value !== query);
-
-            localStorage.setItem("history", JSON.stringify(newHistory));
-            setHistory(newHistory);
-        };
-
-        return [history, add, remove];
+        storage = localStorage;
     }
+    const [history, setHistory] = useState(
+        storage.getItem("history") ? JSON.parse(storage.getItem("history")) : []
+    );
+
+    const add = (query) => {
+        const newHistory = [...history, query];
+        if (newHistory.length > 100) {
+            newHistory.pop();
+        }
+
+        storage.setItem("history", JSON.stringify(newHistory));
+        setHistory(newHistory);
+    };
+
+    const remove = (query) => {
+        const newHistory = history.filter((value) => value !== query);
+
+        storage.setItem("history", JSON.stringify(newHistory));
+        setHistory(newHistory);
+    };
+
+    return [history, add, remove];
 };
 
 export default useSearchHistory;
